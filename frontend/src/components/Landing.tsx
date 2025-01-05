f2a5f74224ce9b18f6d8979718b9f656423bc29f
Vaibhav Rathore <vishurathore200@gmail.com>
2025-01-05 13:28:48 +0530

    frontend inital code for room and landing

---
+++ frontend/src/components/Landing.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+export const Landing = () => {
+
+    const [name, setName] = useState('');
+
+  return (
+    <div>
+        <input type="text"  onChange={(e)=>{
+            setName(e.target.value); //change name 
+        }}/>
+        <Link to={`/room/?name=${name}`}>Join</Link> {/*Link to room with name as search param*/}
+    </div>
+  )
+}
---
+++ frontend/src/components/Room.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+
+export const Room = () => {
+
+    //Returns a tuple of the current URL's URLSearchParams and a function to update them. Setting the search params causes a navigation.
+    const [searchParams, setSearchParams] = useSearchParams(); //get search params
+    const name = searchParams.get('name'); //get name from search params
+
+    useEffect(()=>{ //useEffect to run only once
+        //login to init user to room
+    }, [name]) //run when name changes
+
+  return (
+    <div>
+        Hi {name} //display name
+    </div>
+  )
+}
